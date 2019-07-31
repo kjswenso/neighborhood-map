610d8ccad6c1af83adc21b92198eb60d76b688ad
Katrinka Swenson <kjswenso@gmail.com>
2019-07-31 13:34:45 -0500

    Updates

---
+++ precache-manifest.ec7138a57e33a97352caafc483d8c625.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "2341d3e305021ff34058",
+    "url": "/myapp/static/js/main.2341d3e3.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/myapp/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "2341d3e305021ff34058",
+    "url": "/myapp/static/css/main.ec1a4a08.chunk.css"
+  },
+  {
+    "revision": "9d6377241c0eb9f61ca1119abe111ef2",
+    "url": "/myapp/index.html"
+  }
+];
